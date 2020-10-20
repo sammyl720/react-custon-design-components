23cd7b25e65c432ee290bcd3794214fec370f2b5
sam <sam.leider@hotmail.com>
2020-10-20 12:48:15 -0400

    added table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Card from './components/Card/Card';
+import React, {useEffect, useState } from 'react';
 import Container from './components/container/Container'
-import axios from 'axios'
-import { Child, Welcome } from './Reddit';
-import Grid from './components/Grid/Grid';
 import FlexBox from './components/FlexBox/FlexBox';
-import CardFooter from './components/Card/CardFooter/CardFooter';
-import Pill from './components/Pill/Pill';
+import Table from './components/Table/Table';
+import { dataType, fetchUsers } from './api/users'
 function App() {
-  
+  const [data, setData] = useState<dataType | null>(null)
+  useEffect(() => {
+    fetchUsers(setData)
+  }, [])
   return (
-    <div style={{width:'100%', height: '100%'}}>
-        <Container fluid addClasses='primary'>
-          <FlexBox direction='row' alignItems='alignCenter' justifyContent='justifyCenter'>
-              <h1 style={{ fontSize: 72, fontWeight: 200 }}>Hello World!</h1>
-              <Pill color='classic'>Primary</Pill>
-          </FlexBox>
-        </Container>
-          <Container fluid addClasses='success'>
-          <FlexBox direction='row' alignItems='alignCenter' justifyContent='justifyCenter'>
-              <h1 style={{ fontSize: 72, fontWeight: 200 }}>Hello World!</h1>
-              <Pill color='primary'>success</Pill>
-          </FlexBox>
-        </Container>
-          <Container fluid addClasses='classic'>
-          <FlexBox direction='row' alignItems='alignCenter' justifyContent='justifyCenter'>
-              <h1 style={{ fontSize: 72, fontWeight: 200 }}>Hello World!</h1>
-              <Pill color='secondary'>Classic</Pill>
-          </FlexBox>
-        </Container>
-        <Container fluid addClasses='secondary'>
-          <FlexBox direction='row' alignItems='alignCenter' justifyContent='justifyCenter'>
-              <h1 style={{ fontSize: 72, fontWeight: 200 }}>Hello World!</h1>
-              <Pill color='classic'>Secondary</Pill>
-          </FlexBox>
-        </Container>
-        <Container fluid addClasses='warning'>
-          <FlexBox direction='row' alignItems='alignCenter' justifyContent='justifyCenter'>
-              <h1 style={{ fontSize: 72, fontWeight: 200 }}>Hello World!</h1>
-              <Pill color='danger'>Warning</Pill>
-          </FlexBox>
-        </Container>
-          <Container fluid addClasses='danger'>
-          <FlexBox direction='row' alignItems='alignCenter' justifyContent='justifyCenter'>
-              <h1 style={{ fontSize: 72, fontWeight: 200 }}>Hello World!</h1>
-              <Pill color='warning'>Danger</Pill>
-          </FlexBox>
-        </Container>
-    </div>
+    <Container fluid addClasses='classic'>
+      <FlexBox
+        alignItems='alignCenter'
+        justifyContent='justifyCenter'
+        direction='column'
+      >
+        {data && (
+          <Table color='warning' headers={Object.keys(data[0])} data={data} />
+        )}
+      </FlexBox>
+    </Container>
   );
 }
 
