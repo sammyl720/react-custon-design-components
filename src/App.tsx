7539b1d4d7f786999491fb9e7e7549f1cb68a5b7
sam <sam.leider@hotmail.com>
2020-10-26 13:08:47 -0400

    added AppBar Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,29 @@ import Container from './components/container/Container'
 import FlexBox from './components/FlexBox/FlexBox';
 import Table from './components/Table/Table';
 import { dataType, fetchUsers } from './api/users'
+import Grid from './components/Grid/Grid';
+import AppBar from './components/AppBar/AppBar';
+
+const List= () =>  {
+  return (
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+    </ul>
+  )
+}
 function App() {
   const [data, setData] = useState<dataType | null>(null)
   useEffect(() => {
     fetchUsers(setData)
   }, [])
   return (
-    <Container fluid addClasses='classic'>
-      <FlexBox
-        alignItems='alignCenter'
-        justifyContent='justifyCenter'
-        direction='column'
-      >
-        {data && (
-          <Table color='warning' headers={Object.keys(data[0])} data={data} />
-        )}
-      </FlexBox>
+    <Container fluid>
+      <AppBar color='danger' Logo='imgs/logos/logo1.png' Links={List} />
     </Container>
   );
 }
